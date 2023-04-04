b6c68b15b13d41268ceb277c4eca09757ed3df4b
joak990 <joakhaidar@gmail.com>
2023-04-04 11:02:01 -0300

    Node

--- 01-Node/homework/01 - Exercises/bash.js
+++ 01-Node/homework/01 - Exercises/bash.js
@@ -2,10 +2,27 @@ const process = require('process');
 const { Z_ASCII } = require('zlib');
 const commands = require('./commands/index.js');
 
-function bash() {}
+ 
+function bash() {
+   process.stdout.write("prompt > ")
+   process.stdin.on('data',function(data) {
+  let args = data.toString().trim()
+  let cmd = args.split(' ')[0]
+  if (cmd in commands){
+   commands[cmd](print,args)
+  }else {
+   print(`command not found: ${cmd}`)
+  }
+   });}
+
+function print(output) {
+process.stdout.write(output +'\n')
+process.stdout.write('\nprompt > ')
+}
 
 bash();
 module.exports = {
    print,
    bash,
+  
 };
--- 01-Node/homework/01 - Exercises/commands/index.js
+++ 01-Node/homework/01 - Exercises/commands/index.js
@@ -1,21 +1,75 @@
 const fs = require("fs");
 const utils = require("../utils/request");
 const process = require("process");
+const { throws } = require("assert");
+const { request } = require("../utils/request");
 
-function pwd() {}
+function pwd(print) {
+   TransformStream(process.cwd())
+}
 
-function date() {}
+function date(print) {
+    print(new Date().toString())
+}
 
-function echo() {}
+function echo(print,args) {
+print(args)
+}
 
-function ls() {}
+function ls(print) {
+    const fs = require('fs');
+    fs.readdir(".",(error,files)=>{
+     if(error) {
+     throw error
+    }  
+    print(files.join(' '))
+     
+})
+}
 
-function cat() {}
+function cat(print,args) {
+    fs.readFile(args, 'utf-8',(error,data)=>{
+        if(error){
+            throw error
+        }
+    print(data)
+    } 
+    )
+}
 
-function head() {}
+function head(print,args) {
+    fs.readFile(args,'utf-8',(error,data)=>{
+        if(error){
+            throw error
+        }
+        const datita = data.split('\n')[0]
+    print(datita)
+    }
 
-function tail() {}
+    )
+}
 
-function curl() {}
+function tail(print,args) {
+    fs.readFile (args,'utf-8',(error,data)=>{
+        if(error){
+            throw error
+        } 
+      const datin1 = data.split('\n')
+      const datinfinish=[datin1.length -1]  
+        print(datinfinish)
+    }
 
-module.exports = {};
+    )
+}
+
+function curl(print,args) {
+    request(args,(error,response)=>{
+        if(error){
+            throw error
+        }else{
+            print(response)
+        }
+  })
+}
+
+module.exports = {pwd,date,echo,ls,cat,head,tail,curl};
